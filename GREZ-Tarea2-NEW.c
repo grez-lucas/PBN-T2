a4585e2ad827b7e72cc199918c0b0162633abe66
grez-lucas <lgrez1@miuandes.cl>
2022-04-30 21:03:54 -0400

    feat(began topHero() implementation): search for target hero and search for topHeroes logic

--- GREZ-Tarea2-NEW.c
+++ GREZ-Tarea2-NEW.c
@@ -111,17 +111,103 @@ struct hero *buildDB(){
     return hero_arr;
 }
 
+struct hero *topHero(struct hero * database, char* target_name, char* attribute){
+    int input;
+    int betterBy = 1;
+    int selectedAttribute;
+    static struct hero * hero_arr;
+    struct hero target_hero;
 
+    struct heroQuery{
+        char* name;
+        int attribute;
+    };
+    //search for target hero
+    for(int i=0;i<731;i++){
+        if( strcmp( database[i].name , target_name) == 0 ) {
+            target_hero = database[i]; // WATCHOUT for what is a copy and what is database info
+            break;
+        };      
+    }
+    hero_arr = calloc(10, sizeof(struct hero) );
+    //search for topHeroes
+    int counter = 0;
+    if (strcmp(attribute, "id") == 0) selectedAttribute = 0;
+    else if (strcmp(attribute, "intelligence") == 0) selectedAttribute = 1;
+    else if (strcmp(attribute, "strength") == 0) selectedAttribute = 2;
+    else if (strcmp(attribute, "speed") == 0) selectedAttribute = 3;
+    else if (strcmp(attribute, "durability") == 0) selectedAttribute = 4;
+    else if (strcmp(attribute, "power") == 0) selectedAttribute = 5;
+    else if (strcmp(attribute, "combat") == 0) selectedAttribute = 6;
+    else selectedAttribute = 7;
+    while ( hero_arr[9].id == 0 && selectedAttribute != 7 )
+    {
+        switch(selectedAttribute){
+            case 0:
+            for(int i =0;i<731;i++){
+                if( database[i].id - target_hero.id == betterBy){
+                    hero_arr[counter] = database[i];
+                } 
+            }
+            break;
+            case 1:
+            for(int i =0;i<731;i++){
+                if( database[i].intelligence - target_hero.intelligence == betterBy){
+                    hero_arr[counter] = database[i];
+                } 
+            }
+            break;
+            case 2:
+            for(int i =0;i<731;i++){
+                if( database[i].strength - target_hero.strength == betterBy){
+                    hero_arr[counter] = database[i];
+                } 
+            }
+            break;
+            case 3:
+            for(int i =0;i<731;i++){
+                if( database[i].speed - target_hero.speed == betterBy){
+                    hero_arr[counter] = database[i];
+                } 
+            }
+            break;
+            case 4:
+            for(int i =0;i<731;i++){
+                if( database[i].durability - target_hero.durability == betterBy){
+                    hero_arr[counter] = database[i];
+                } 
+            }
+            break;
+            case 5:
+            for(int i =0;i<731;i++){
+                if( database[i].power - target_hero.power == betterBy){
+                    hero_arr[counter] = database[i];
+                } 
+            }
+            break;
+            case 6:
+            for(int i =0;i<731;i++){
+                if( database[i].combat - target_hero.combat == betterBy){
+                    hero_arr[counter] = database[i];
+                } 
+            }
+            break;
+            
+        counter ++; //syntax may be wrong..
+        }
+        betterBy +=1;
+        
+    }
 
-
-// create getValuefromMemory function
+    printf("de que superheroe desea ver su informacion?\n");
+    scanf( "%d", &input);
+    return hero_arr;
+}
 
 int main(int argc, char *argv)
 {
-    //char line[] ="1;A-Bomb;38;100;17;80;24;64;Richard Milhouse Jones;No alter egos found.;Rick Jones;Scarsdale; Arizona;Hulk Vol 2 #2 (April; 2008) (as A-Bomb);Marvel Comics;good;Male;Human;6'8;203 cm;980 lb;441 kg;Yellow;No Hair;Musician; adventurer;";
-    //struct hero hero1 = getHero(line);
+
     struct hero* database = buildDB();
-    //printf("%s \n", hero1.name);
     for(int i =0; i<5; i++){
         printf("%d ", database[i].id);
         printf("%s \n", database[i].name);
